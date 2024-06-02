6804090f4b899eb0282f837d038d6008d985957b
Amir Navon <amirnavon3@gmail.com>
2024-06-02 20:33:38 +0300

    executing the api
    now the script is connected to web

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import pandas as pd
+import requests
 
-s = pd.Series([1,2,3])
-print(s)
+api_key = 'a2ea41563a89e46d5877cb84f24215d5'
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}"
+)
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+    humidity = weather_data.json()['main']['humidity']
+    print(f"The weather now in {user_input.capitalize()} is {weather}.\nthe temperature is {temp} \u00b0F\nand the "
+          f"humidity is {humidity}")
